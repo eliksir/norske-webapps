ae147988fe628b718dcb92821ddd1b676af72e00
Frode Danielsen <frode@e5r.no>
2012-10-16 20:04:39 +0200

    Preparing for better deploy structure

---
+++ install/includes/config_default.php
@@ -0,0 +1,90 @@
+<?php
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'pagination_maximum_rows' => '20',
+			'lang' => 'en',
+			'version' => '2.3.1RC1',
+			'pages_table_nest_children' => 'yes',
+			'session_gc_divisor' => '10',
+			'cookie_prefix' => 'sym-',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'sitename' => 'Symphony CMS',
+			'useragent' => 'Symphony/2.3.1RC1',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'query_caching' => 'default',
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => null,
+			'password' => null,
+			'db' => null,
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'yes',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'H:i',
+			'date_format' => 'd F Y',
+			'datetime_separator' => ' ',
+			'timezone' => 'UTC',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0644',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0755',
+		),
+		########
+	);
